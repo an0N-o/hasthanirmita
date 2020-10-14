9157b2e74ff7194ee7bd807987875509d15b9909
an0N-o <39913238+an0N-o@users.noreply.github.com>
2020-10-14 21:07:47 +0530

    Add files via upload

---
+++ assets/cardSliderProducts/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    $("#news-slider-durga").owlCarousel({
+        items : 4,
+        itemsDesktop:[1199,3],
+        itemsDesktopSmall:[980,2],
+        itemsMobile : [600,1],
+        navigation:true,
+        navigationText:["",""],
+        pagination:true,
+        autoPlay:true
+    });
+});
+
+
+$(document).ready(function() {
+    $("#news-slider-alterego").owlCarousel({
+        items : 4,
+        itemsDesktop:[1199,3],
+        itemsDesktopSmall:[980,2],
+        itemsMobile : [600,1],
+        navigation:true,
+        navigationText:["",""],
+        pagination:true,
+        autoPlay:true
+    });
+});
+
+
+$(document).ready(function() {
+    $("#news-slider-cottonCandy").owlCarousel({
+        items : 4,
+        itemsDesktop:[1199,3],
+        itemsDesktopSmall:[980,2],
+        itemsMobile : [600,1],
+        navigation:true,
+        navigationText:["",""],
+        pagination:true,
+        autoPlay:true
+    });
+});
